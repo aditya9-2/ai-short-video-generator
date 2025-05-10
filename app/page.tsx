6ad3cdb9764e5be39cabb0d394a9240e209acb22
Aditya <adityabasak7031@gmail.com>
2025-05-10 23:26:13 +0530

    add user Burron

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,21 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <div className="">
       hi there
       <Button>Click</Button>
+
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+
     </div>
   );
 }
