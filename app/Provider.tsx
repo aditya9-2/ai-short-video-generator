5218ee40f1a4f18380bfb371a874e4dd720d4af1
Aditya <adityabasak7031@gmail.com>
2025-05-10 23:25:43 +0530

    add function to insert data in DB

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,10 +1,40 @@
+"use client"
+
+import { users } from "@/configs/schema";
+import { useUser } from "@clerk/nextjs";
+import { useEffect } from "react";
+import { db } from "@/configs/db";
+import { eq } from "drizzle-orm";
+
 export const Provider = ({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) => {
+
+    const { user } = useUser();
+
+    useEffect(() => {
+        if (user) {
+            const isNewUser = async () => {
+                const clerkEmail = user?.primaryEmailAddress?.emailAddress;
+                if (!clerkEmail) return;
+
+                const result = await db.select().from(users).where(eq(users.email, clerkEmail))
+
+                if (!result[0]) {
+                    await db.insert(users).values({
+                        username: user.username || "",
+                        email: clerkEmail,
+                        imageUrl: user.imageUrl
+                    })
+                }
+            }
+            isNewUser();
+        }
+    }, [user])
+
     return (
         <div>{children}</div>
     )
 }
-
