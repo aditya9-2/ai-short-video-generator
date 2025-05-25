6224d8c9a7c3359a251aff330ce62d54104cc1c1
Aditya <adityabasak7031@gmail.com>
2025-05-25 17:33:36 +0530

    re-design auth page

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,10 +1,25 @@
-import GradientBackground from '@/components/GradientBackground'
 import { SignUp } from '@clerk/nextjs'
+import Image from 'next/image'
 
 export default function Page() {
     return (
-        <GradientBackground>
-            <SignUp />
-        </GradientBackground>
+        <div className='h-screen w-full flex items-center justify-between 
+                        bg-gradient-to-br from-[#141130] via-[#272261] to-[#333358] 
+                        text-white'>
+
+            <div>
+                <Image
+                    src={'/landing.png'}
+                    alt='landing'
+                    width={486}
+                    height={250}
+                    className='hidden md:block max-w-full h-auto object-contain'
+                    priority
+                ></Image>
+            </div>
+            <div className='md:px-[7rem] p-10 flex justify-center items-center'>
+                <SignUp />
+            </div>
+        </div>
     )
 }
