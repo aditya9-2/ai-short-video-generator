21d30b0f6647f430cf22dcfb3307bfb052167d83
Aditya <adityabasak7031@gmail.com>
2025-05-25 19:48:49 +0530

    fix header

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -9,11 +9,13 @@ const DashboardLayout = ({
 }>) => {
     return (
         <div className='min-h-screen bg-gray-100'>
-            <div className='hidden md:block h-screen bg-white fixed left-0 mt-[78px] w-64 border-[1px]'>
+            <div className='hidden md:block h-screen bg-white fixed left-0 mt-[70px] w-64 border-[1px]'>
                 <SideNav />
             </div>
             <div>
-                <Header />
+                <div className='fixed top-0 left-0 w-full'>
+                    <Header />
+                </div>
                 <div className='md:ml-65 md:mt-2 p-4'>
                     {children}
                 </div>
