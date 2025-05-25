59e04fbfe31921d18556c101a09166ea52c5799e
Aditya <adityabasak7031@gmail.com>
2025-05-25 17:03:38 +0530

    add EmpltyList component

---
+++ app/dashboard/_components/EmptyList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EmptyList = () => {
+    return (
+        <div className='text-gray-400 text-center flex justify-center items-center'>
+            <p className='mt-[10rem]'>{"hmm, seems Like you have'nt created any video yet!"}</p>
+        </div>
+    )
+}
+
+export default EmptyList
