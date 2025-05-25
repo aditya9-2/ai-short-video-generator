128d3b0255790c9488c7e78d8c0e2ba1235bc02f
Aditya <adityabasak7031@gmail.com>
2025-05-25 16:38:24 +0530

    add sidebar

---
+++ app/dashboard/_components/SideNav.tsx
@@ -0,0 +1,49 @@
+import { CircleUserIcon, FileVideo, PanelsTopLeft, ShieldPlus } from 'lucide-react'
+import Link from 'next/link'
+import React from 'react'
+
+const SideNav = () => {
+    const options = [
+        {
+            id: 1,
+            name: 'Dashboard',
+            path: '/dashboard',
+            icon: PanelsTopLeft
+        },
+        {
+            id: 2,
+            name: 'Create New Vide',
+            path: '/create-new',
+            icon: FileVideo
+        },
+        {
+            id: 3,
+            name: 'Upgrade',
+            path: '/upgrade',
+            icon: ShieldPlus
+        },
+        {
+            id: 4,
+            name: 'My Account',
+            path: '/account',
+            icon: CircleUserIcon
+        },
+    ]
+    return (
+        <div className='w-64 h-screen shadow-md p-2'>
+            <div className='grid gap-2'>
+                {options.map((menu) => (
+                    <Link href={menu.path} key={menu.id} target='_blank'>
+                        <div className='flex gap-3 items-center p-3 hover:bg-secondary hover:cursor-pointer hover:rounded-md'>
+                            <menu.icon />
+                            <h2>{menu.name}</h2>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default SideNav
