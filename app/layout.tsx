46728250c3ef6fb02c5e5055b3ab8f217f173629
Aditya <adityabasak7031@gmail.com>
2025-05-25 15:55:42 +0530

    change text style

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google"
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Provider } from "./Provider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const outfit = Outfit({
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "AI short video generator",
@@ -28,7 +22,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+          className={`${outfit.className} antialiased`}
         >
           <Provider>
             {children}
