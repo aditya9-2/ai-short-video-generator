1213ee6e9c4e9127a917f00124490e0403e64b99
Aditya <adityabasak7031@gmail.com>
2025-05-28 13:06:37 +0530

    add dependency uuid

--- app/dashboard/create-new/page.tsx
+++ app/dashboard/create-new/page.tsx
@@ -8,13 +8,24 @@ import { Button } from '@/components/ui/button'
 import React, { useState } from 'react'
 import axios from 'axios';
 import CustomLoading from '@/components/CustomLoading'
+import { v4 as uuidv4 } from 'uuid';
+
+const Sdata = ` Scene 1: A tiny cat chef begins preparing a gourmet meal. Scene 2: A dog is unsuccessfully trying to catch its tail.  Scene 3: Animals having an elegant tea party. Scene 4: A squirrel struggles to open a nut. Scene 5: An unlikely underwater bicycle ride. Scene 6: A superhero rabbit battles a villainous carrot. Scene 7: Unlikely friends share a pizza slice. Scene 8: A clumsy bear tries using a smartphone. Scene 9: A penguin slips on a banana peel. Scene 10: An unexpected race winner. 
+﻿
+
+s`
 
 const CreateNew = () => {
+
+    type Scene = {
+        imagePrompt: string;
+        ContentText: string
+    };
 
     const [formData, setFormData] = useState<Record<string, any>>({});
 
     const [loading, setLoading] = useState<boolean>(false);
-    const [videoScript, setVideoScript] = useState<string | undefined>();
+    const [videoScript, setVideoScript] = useState<Scene[]>();
 
     const onHandleInput = (fieldName: string, fieldValue: any) => {
 
@@ -25,7 +36,7 @@ const CreateNew = () => {
 
     }
 
-    // API for video script
+    // API Call for video script
     const getVideoScript = async () => {
         const totalPrompt = `write a script to generate ${formData.duration} video on topic: ${formData.topic} along with AI image prompt in ${formData.imageStyle} format for each scene and give me result in JSON format with imagePrompt and ContentText as field`;
 
@@ -34,19 +45,46 @@ const CreateNew = () => {
 
         try {
             setLoading(true);
-            const response = await axios.post('/api/get-video-script', {
+            const response = await axios.post<{ result: Scene[] }>('/api/get-video-script', {
                 prompt: totalPrompt
             });
 
-            const data = response.data;
-
+            // const data = response.data;
             // console.log(`data: ${JSON.stringify(data, null, 2)}`);
-
-            setVideoScript(JSON.stringify(data, null, 2));
+            const scenes = response.data.result;  // extract the array
+            setVideoScript(scenes);
+            generateAudioFile(scenes);
 
         } catch (err) {
 
             console.log(`Error occured while call getVideo API: ${err}`);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    // API Call for audio script
+    const generateAudioFile = async (scenes: Scene[] | string) => {
+        const script = '';
+        const id = uuidv4();
+        // scenes.forEach(item => {
+        //     script += item.ContentText + ' ';
+        // });
+
+        console.log(`Audio: ${script}`);
+        try {
+            setLoading(true)
+            const response = await axios.post('/api/generate-audio', {
+                text: Sdata,
+                id: id
+            });
+
+            const data = response.data;
+            console.log(data);
+            setLoading(false);
+        } catch (err) {
+
+            console.log(`Error while call Audio API: ${err}`)
         } finally {
             setLoading(false);
         }
@@ -69,7 +107,7 @@ const CreateNew = () => {
                 <div className='flex justify-center items-center'>
                     <Button
                         className='mt-10 w-48 hover:cursor-pointer'
-                        onClick={getVideoScript}
+                        onClick={() => generateAudioFile(Sdata)}
 
                     >
                         Generate
