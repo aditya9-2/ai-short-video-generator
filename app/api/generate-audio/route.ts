78973ce8b726b4edd5a1c8c35d23b5b61ba049b5
Aditya <adityabasak7031@gmail.com>
2025-05-28 12:11:03 +0530

    create audio route BE

---
+++ app/api/generate-audio/route.ts
@@ -0,0 +1,33 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import textToSpeech from "@google-cloud/text-to-speech"
+import { NextResponse } from "next/server";
+
+const client = new textToSpeech.TextToSpeechClient({
+    apiKey: process.env.GOOGLE_TEXT_TO_SPEECH_API_KEY
+});
+
+export async function POST(req: Request) {
+
+    const { text } = await req.json();
+
+    const request = {
+        input: { text: text },
+        // Select the language and SSML voice gender (optional)
+        voice: { languageCode: 'en-US', ssmlGender: 'FEMALE' },
+        // select the type of audio encoding
+        audioConfig: { audioEncoding: 'MP3' },
+    };
+
+    // Performs the text-to-speech request
+    // @ts-ignore
+    const [response] = await client.synthesizeSpeech(request);
+    // Write the binary audio content to a local file
+    // @ts-ignore
+    const writeFile = util.promisify(fs.writeFile);
+    await writeFile('output.mp3', response.audioContent, 'binary');
+    console.log('Audio content written to file: output.mp3');
+    return NextResponse.json({
+        Result: 'Successful mp3 conversion'
+    })
+
+}
